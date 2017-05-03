25f027b7fa312edf67940733d1d6efafdebb9069
Sara <sklvgirl@gmail.com>
2017-05-02 18:00:33 -0700

    adding flask_pipeline

--- app/flask_pipeline.py
+++ app/flask_pipeline.py
@@ -5,31 +5,33 @@ import pymongo, json
 import argparse
 
 
-class AppBack():
-    '''Creates flask app backbone.
-    '''
 
-    app = Flask(__name__)
-    @app.route('/')
+'''Creates flask app backbone.
+'''
 
-    def predict(data):
-        """Recieve the data from input form and use model to predict.
-        """
-        pred = model.predict(data)
-        proba = model.predict_proba(data)
-        return jsonify((pred,proba),data)
+app = Flask(__name__)
+@app.route('/')
 
-    if __name__ == '__main__':
-        parser = argparse.ArgumentParser(
-            description='.')
-        parser.add_argument('--data', help='The input data')
-        parser.add_argument('--out', help='A file to save the pickled model object to.')
-        args = parser.parse_args()
+def predict(data):
+    """Recieve the data from input form and use model to predict.
+    """
+    pred = model.predict(data)
+    proba = model.predict_proba(data)
+    return jsonify((pred,proba),data)
 
-        X, y = get_data(args.data)
-        m = model()
-        app.run(host='0.0.0.0', debug=True)
-        with open(args.out, 'w') as f:
-            pickle.dump(m, f)
-        with open('model/pipeline.pkl') as f:
-            model = pickle.load(f)
+#will have data pushed to
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='.')
+    parser.add_argument('--data', help='The input data')
+    parser.add_argument('--out', help='A file to save the pickled model object to.')
+    args = parser.parse_args()
+
+    X, y = get_data(args.data)
+    m = model()
+    app.run(host='0.0.0.0', debug=True)
+    with open(args.out, 'w') as f:
+        pickle.dump(m, f)
+    with open('model/pipeline.pkl') as f:
+        model = pickle.load(f)
